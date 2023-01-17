26db5604adfa0075e480c64ddf89e11a98b1dc40
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-01-17 19:43:45 +0100

    Revert "Solution Task 2"
    This reverts commit e69af956ea976fe8f299ef18d899525c12643db4.

--- src/main/kotlin/lms_126/Task2.kt
+++ src/main/kotlin/lms_126/Task2.kt
@@ -2,10 +2,5 @@ package lms_126
 
 fun task2(): String {
 
-    return """
-      INPUT base of rectangle 
-      INPUT height of rectangle
-      Calculate area by multiplying base times height
-      OUTPUT area of rectangle
-    """
+    return TODO("Replace this 'TODO' with your solution")
 }
