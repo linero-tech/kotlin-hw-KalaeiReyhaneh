947c009c3717b251261c085672ddb95849cef4da
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-01-31 14:50:37 +0100

    C TO F

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        Convert temperatures from Celcius to Farenheit
+        INPUT F
+        STEP C = (F - 32) / (1.8)
+        print C
+        INPUT C
+        STEP F = (C * 1.8) + 32 
+        print F 
+        
+    """
+        .trimIndent()
 }
