9b4b4d608400e2de530ea49bccc395031ae19c3d
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-01-31 13:39:48 +0100

    101 Task 8

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -3,10 +3,19 @@ package lms_126
 fun task8(): String {
 
     return """
+     FOR number X
+       IF X % 3 == 0 :
+          print "Foo"
      FOR number x
-       IF x % 3 == 0 :
-          print Foo
-     FOR number x
-       IF X
-    """.trimIndent()
+       IF X % 5 == 0 :
+          print "Bar"
+     FOR number X
+       IF X % 3 & % 5 == 0 :
+          print "FooBar"
+     FOR number X 
+       IF X % 3 or % 5 == 0 :
+          print "Qix"
+          
+    """
+        .trimIndent()
 }
