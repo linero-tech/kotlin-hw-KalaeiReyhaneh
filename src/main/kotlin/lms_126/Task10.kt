48d64285da70aff8932e87a1098c47d1d51a4a4f
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-01-31 14:08:49 +0100

    Creat an account

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,14 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        Create an account for Email
+        ENTER your name
+        ENTER your surname 
+        ENTER your birthday
+        ENTER your username
+        ENTER your password
+        Enter confirm
+    """
+        .trimIndent()
 }
