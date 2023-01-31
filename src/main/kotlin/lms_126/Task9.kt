dcd97956456872e6ee1ed173e8130fe4f3ab8ba3
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-01-31 13:59:34 +0100

    ATM Works

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,12 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       WELCOME TO KOTLIN BANK ATM
+        ENTER your PIN CODE
+        INPUT How much would you like to withdraw today?
+        ENTER your amount
+           print the receipt
+        """
+        .trimIndent()
 }
