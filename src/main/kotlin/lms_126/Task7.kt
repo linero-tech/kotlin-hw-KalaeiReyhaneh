138de05de7203a37492ba0a61753eabbdd594640
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-02-05 15:38:41 +0100

    Task 7

--- src/main/kotlin/lms_126/Task7.kt
+++ src/main/kotlin/lms_126/Task7.kt
@@ -1,11 +1,14 @@
 package lms_126
 
-fun task7() {
-       """ FOR number in range (100) :
+fun task7(): String {
+
+    return """
+     FOR number in range (100) :
               IF number % 2 == 0 :
                  print number
        
        """
+
 
            .trimIndent()
 
