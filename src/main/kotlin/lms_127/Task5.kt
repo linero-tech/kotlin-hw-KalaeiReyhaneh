9522f346f810a87ccde08324aa59ec2099902d6a
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-02-21 13:56:12 +0100

    Module102 Task5

--- src/main/kotlin/lms_127/Task5.kt
+++ src/main/kotlin/lms_127/Task5.kt
@@ -5,8 +5,25 @@ fun task5(valueForA: Int, valueForB: Int): Pair<Int, Int> {
     var a = valueForA
     var b = valueForB
 
-    TODO("Your solutions goes in here.")
+    //println("Enter first integer number:")
+    //val valueForA = readln()
+    //println("Enter second integer number:")
+    //val valueForB = readln()
+
+    println("before swap:a =  $a, b: $b")
+
+
+    var c = a
+    a = b
+    b = c
+
+    println("After Swap: a = $a, b: $b")
+
+
 
     // Do not erase or change this statement
     return Pair(a, b)
+}
+fun main()  {
+    val result = task5(4,2)
 }
