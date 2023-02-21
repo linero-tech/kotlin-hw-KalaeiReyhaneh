d9055acddee8ec10e9833ba814934dd2b1551881
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-02-21 13:57:39 +0100

    Module102 Task10

--- src/main/kotlin/lms_127/Task10.kt
+++ src/main/kotlin/lms_127/Task10.kt
@@ -1,9 +1,9 @@
 package lms_127
 
-fun task10_1(assessments: String): Int {
+//fun task10_1(assessments: String): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+    //return 'result'
+//}
 
 fun task10_2(assessments: String): Char {
 
