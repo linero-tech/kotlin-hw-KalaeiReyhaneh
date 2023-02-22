c54c9fd6a6918e9b655964dca29761e75261837c
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-02-22 11:38:04 +0100

    Module102 Task7

--- src/main/kotlin/lms_127/Task7.kt
+++ src/main/kotlin/lms_127/Task7.kt
@@ -4,13 +4,14 @@ import kotlin.jvm.internal.Intrinsics.Kotlin
 
 fun task7(sentence: String): Int {
 
-val sentence = "I love kotlin"
+val sentence = "I love GBG"
     val numberOfCharacters = sentence.length
     //println("The sentence has $numberOfCharacters characters")
     return numberOfCharacters
 }
 fun main()  {
-    val result = task7("I love kotlin")
+    val result = task7("I love GBG")
     println("The sentence has $result characters")
+
 
 }
