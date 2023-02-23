9389ee66f6d06cbcebf156340d6838f2524196df
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-02-23 12:05:19 +0100

    Module102 Task6

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -5,7 +5,7 @@ fun task6(base1: Double, base2: Double, height: Double): Double {
     val base1 = 10.0
     val base2 = 20.0
     val height = 1.0
-    
+
 
   val result = (base1 + base2) * height / 2
    return result
