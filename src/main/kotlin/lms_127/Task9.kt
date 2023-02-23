87c772840bcf82cc7735f03379ee01a48597725f
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-02-23 12:02:25 +0100

    Module102 Task9

--- src/main/kotlin/lms_127/Task9.kt
+++ src/main/kotlin/lms_127/Task9.kt
@@ -5,6 +5,7 @@ fun task9(sentence: String, character: Char): Boolean {
     val result = sentence.contains('i', ignoreCase = true)
     return result
 }
+
 fun main ()  {
     val result = task9("I code In KOTLIN",'i')
     println("The letter is present $result times")
