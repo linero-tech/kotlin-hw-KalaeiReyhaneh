6a0e3cb314aac2651ad8f5b201bb92f84506eb3d
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-02-28 14:02:25 +0100

    Module102 Task1

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,12 +1,14 @@
 package lms_127
 
 fun task1() {
+
     val countryName = "Panama"
     val capitalCity = "Panama city"
     val currency = "USD"
     println("Country: $countryName")
     println("Capital: $capitalCity")
     println("Currency: $currency")
+    return
 }
 
 fun main ()  {
