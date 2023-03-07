d1c329f77d1f9343d5d30538050a8c863a4130a8
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-03-07 13:50:37 +0100

    Module 103 Task6

--- src/main/kotlin/lms_129/Task6.kt
+++ src/main/kotlin/lms_129/Task6.kt
@@ -2,5 +2,22 @@ package lms_129
 
 fun task6(number: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var num = number
+    var reversed = 0
+
+    while (num != 0) {
+        val digit = num % 10
+        reversed = reversed * 10 + digit
+        num /= 10
+    }
+
+   return reversed
+
+}
+
+fun main()  {
+    val result = task6(678)
+    println("$result")
+
+
 }
