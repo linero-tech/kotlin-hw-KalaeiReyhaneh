6c4ef9e4b530dbab54367b970c51591e00dd4afa
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-03-07 13:50:21 +0100

    Module 103 Task5

--- src/main/kotlin/lms_129/Task5.kt
+++ src/main/kotlin/lms_129/Task5.kt
@@ -1,6 +1,14 @@
 package lms_129
 
 fun task5(sentence: String): Int {
+    val sentence = "I love GBG"
+    val numberOfCharacters = sentence.length
+    //println("The sentence has $numberOfCharacters characters")
+    return numberOfCharacters
+}
+fun main()  {
+    val result = lms_127.task7("I love GBG")
+    println("The sentence has $result characters")
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
 }
