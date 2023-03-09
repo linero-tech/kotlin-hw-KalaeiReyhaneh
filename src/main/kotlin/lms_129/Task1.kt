69c618cf62483093d7b6b24cf880bc18a3e12ca3
KalaeiReyhaneh <122488205+KalaeiReyhaneh@users.noreply.github.com>
2023-03-09 13:51:41 +0100

    Module103 Task1

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -2,20 +2,21 @@ package lms_129
 
 fun task1(a: Int, b: Int): Int {
 
-    var sum = 0
+    var result = 0
 
     if (a<b)
     for (number in a..b)
-        sum += number
 
-    println("sum: $sum")
+        result += number
+
+    println("sum: $result")
 
 
-    return sum
+    return result
 }
 
 fun main() {
-    val result = task1(1,1)
+    val result = task1(1,5)
 
 
 }
